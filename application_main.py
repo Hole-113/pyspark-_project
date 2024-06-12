74d2aec058540a04bbc97e18166e33fc1b01de0c
Hole-113 <amithole113cg@gmal.com>
2024-06-12 07:52:37 +0000

    changes

--- application_main.py
+++ application_main.py
@@ -18,11 +18,11 @@ def main():
     try:
     
         job_run_env = os.environ.get('ENVIRON')
-        print(job_run_env)
-        print("Creating Spark Session")
+        logging.info(job_run_env)
+        logging.info("Creating Spark Session")
         spark = Utils.get_spark_session(job_run_env)
         spark.sparkContext.setLogLevel('ERROR')
-        print("Created Spark Session")
+        logging.info("Created Spark Session")
         orders_df = DataReader.read_orders(spark,job_run_env)
         orders_filtered = DataManipulation.filter_closed_orders(orders_df)
         customers_df = DataReader.read_customers(spark,job_run_env)
--- lib/DataReader.py
+++ lib/DataReader.py
@@ -1,27 +1,47 @@
 from lib import ConfigReader
+import logging
+import logging.config
+logging.config.fileConfig(fname='/home/hadoop/pyspark_project/configs/logging_to_file.conf')
+logger=logging.getLogger('DataReader')
+
+
+
+
+
 #defining customers schema
 def get_customers_schema():
-    schema = "customer_id int,customer_fname string,customer_lname string,username string,password string,address string,city string,state string,pincode string"
-    return schema
-# creating customers dataframe
+        try:
+            logger.info("get_customers_schema() function is started")
+            schema = "customer_id int,customer_fname string,customer_lname string,username string,password string,address string,city string,state string,pincode string"
+        except Exception as exp:
+            logger.error(str(exp),exc_info=True)
+        else:
+            return schema   
+    
+    # creating customers dataframe
 def read_customers(spark,env):
-    conf = ConfigReader.get_app_config(env)
-    customers_file_path = conf["customers.file.path"]
-    return spark.read \
-           .format("csv") \
-           .option("header", "true") \
-           .schema(get_customers_schema()) \
-           .load(customers_file_path)
-#defining orders schema
+    try:
+        logger.info("read_customers() function is started")   
+        conf = ConfigReader.get_app_config(env)
+        customers_file_path = conf["customers.file.path"]
+    except Exception as exp:
+        logger.error("Error in read_customers() method" + str(exp),exc_info=True)
+    else:
+        return spark.read \
+            .format("csv") \
+            .option("header", "true") \
+            .schema(get_customers_schema()) \
+            .load(customers_file_path)
+    #defining orders schema
 def get_orders_schema():
-    schema = "order_id int,order_date string,customer_id int,order_status string"
-    return schema
-#creating orders dataframe
+        schema = "order_id int,order_date string,customer_id int,order_status string"
+        return schema
+    #creating orders dataframe
 def read_orders(spark,env):
-    conf = ConfigReader.get_app_config(env)
-    orders_file_path = conf["orders.file.path"]
-    return spark.read \
-    .format("csv") \
-    .option("header", "true") \
-    .schema(get_orders_schema()) \
-    .load(orders_file_path)
+        conf = ConfigReader.get_app_config(env)
+        orders_file_path = conf["orders.file.path"]
+        return spark.read \
+        .format("csv") \
+        .option("header", "true") \
+        .schema(get_orders_schema()) \
+        .load(orders_file_path)
